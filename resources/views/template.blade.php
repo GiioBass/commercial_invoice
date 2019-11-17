223777a0ab84abae52b86a61d662efe1abb5bed0
William G. Navarro <williamnavarrocanon@gmail.com>
2019-11-17 17:56:19 -0500

    Create template bar navigator

---
+++ resources/views/template.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{asset('css/styles.css')}}">
+    <title>Facturación</title>
+</head>
+<body>
+    <nav class="main-nav">
+        <div class="bar-nav">
+            <ul class="button">
+                <li><a href="#">OPTION 1</a></li>
+                <li><a href="#">OPTION 2</a></li>
+                <li><a href="#">OPTION 3</a></li>
+                <li><a href="#">OPTION 4</a></li>
+                <li><a href="#">OPTION 4</a></li>
+            </ul>
+        </div>
+    </nav>
+    <section>
+        <div class="box"></div>
+    </section>
+    <footer>
+        <a href="{{route('login')}}">Login</a>
+    </footer>
+</body>
+</html>
