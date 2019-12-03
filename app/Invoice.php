1dcb6585078bccca1f5f2bab957460ffab4493ae
William G. Navarro <williamnavarrocanon@gmail.com>
2019-12-03 00:00:55 -0500

    Create relationships

--- app/Invoice.php
+++ app/Invoice.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Invoice extends Model
 {
+    public function client(){
+        return $this->belongsTo(Client::class);
+        
+    }
+
+    public function invoice_products(){
+        return $this->hasMany(Invoice_product::class);
+    }
+
     
+   
 }
