3a21efdb538dc47dcaf94a683c4f3a3726459843
William G. Navarro <williamnavarrocanon@gmail.com>
2019-12-18 21:30:27 -0500

    formatted code of the view - clients index

--- resources/views/Clients/index.blade.php
+++ resources/views/Clients/index.blade.php
@@ -1,34 +1,46 @@
 @extends('template')
 
 @section('content')
-<div class="title"  style="width: 30%; margin:50px auto; text-align: center">
-    <h1>MODULO CLIENTES</h1>
+
+@include('styles')
+
+
+
+<div style="text-align:center; margin:10px">
+    <a href="{{route('client.create')}}">Añadir Cliente</a>
 </div>
 
-@foreach($clients as $client)
-<tr>
-        <div style="width:50%; margin: 0px auto; font-size: 20px">
-            <td>
-                   
-            </td>
-            <td> * {{$client->id}} - </td>
-            <td>{{$client->first_name}} - </td>
-            <td>{{$client->last_name}} - </td>
-            <td>{{$client->phone_number}} - </td>
-            <td>{{$client->address}}  -   </td>
-            <td><a href="/client/{{$client->id}}/edit">Editar /</a></td>
-            <td><a href="/client/{{$client->id}}/confirmDelete">Eliminar</a></td>
-            <br>
-        </tr>
-    </div>
-@endforeach()
-
-<div style="text-align:center; margin:50px">
-    <a href="{{route('client.create')}}">Añadir Nuevo Cliente</a>
+<div>
+    <table>
+        <div>
+            <tr>
+                <th>Identificación</th>
+                <th>Nombres</th>
+                <th>Apellidos</th>
+                <th>Número Tel</th>
+                <th>Dirección</th>
+                <th></th>
+                <th></th>
+            </tr>
+        </div>
+        @foreach($clients as $client)
+            <div style="">       
+                <tr>
+                    <td>{{$client->id}}</td>
+                    <td>{{$client->first_name}}</td>
+                    <td>{{$client->last_name}}</td>
+                    <td>{{$client->phone_number}}</td>
+                    <td>{{$client->address}}</td>
+                    <td><a href="/client/{{$client->id}}/edit">Editar</a></td>
+                    <td><a href="/client/{{$client->id}}/confirmDelete">Eliminar</a></td>
+                    <br>
+                </tr>
+            </div>
+        @endforeach()
+    </table>
 </div>
+
 <div class="container-pagination">
-    
        
-   
 </div>
 @endsection
