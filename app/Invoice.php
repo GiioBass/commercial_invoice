d7ed0cec0818a7415c2a993248b9c726e1f86165
William G. Navarro <williamnavarrocanon@gmail.com>
2019-12-28 14:37:10 -0500

    Update Invoice.php

--- app/Invoice.php
+++ app/Invoice.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Invoice extends Model
 {
+    protected $fillable = [];
+
     public function client(){
         return $this->belongsTo(Client::class);
         
