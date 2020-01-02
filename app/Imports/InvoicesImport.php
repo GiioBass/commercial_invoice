ea4d3af41d880d5c50a027ab4cdc6aee43ee8ff5
William G. Navarro <williamnavarrocanon@gmail.com>
2020-01-02 01:10:11 -0500

    Create import rows

--- app/Imports/InvoicesImport.php
+++ app/Imports/InvoicesImport.php
@@ -15,7 +15,19 @@ class InvoicesImport implements ToModel
     public function model(array $row)
     {
         return new Invoice([
-            //
+            'id' => $row[0],
+            'state' => $row[1],
+            'expedition_date' => $row[2],
+            'expiration_date' => $row[3],
+            'subtotal' => $row[4],
+            'iva' => $row[5],
+            'total' => $row[6],
+            'created_at' => $row[7],
+            'update_at' => $row[8],
+            'seller_id' => $row[9],
+            'client_id' => $row[10],
+
+
         ]);
     }
 }
