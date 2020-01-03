38dab98755a57a54f9c4320a5e8963de6b151cfa
William G. Navarro <williamnavarrocanon@gmail.com>
2020-01-02 21:29:01 -0500

    Update DatabaseSeeder.php

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,15 +14,15 @@ class DatabaseSeeder extends Seeder
     {
 
         
-        // DB::table('clients')->truncate();
-        // DB::table('sellers')->truncate();
-        // DB::table('products')->truncate();
-        // DB::table('invoices')->truncate();
-        // DB::table('invoice_product')->truncate();
+        
         
         factory(App\Client::class, 30)->create();
         factory(App\Seller::class, 30)->create();
+        factory(App\Invoice::class, 30)->create();
         factory(App\Product::class, 30)->create();
+        factory(App\Invoice_product::class, 100)->create();
+
+        
         
     }
 }
