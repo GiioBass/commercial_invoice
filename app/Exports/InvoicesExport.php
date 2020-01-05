7f4717d1ece28d1605a98dd054fd12a5c88bafa6
William G. Navarro <williamnavarrocanon@gmail.com>
2020-01-04 19:52:41 -0500

    Create export files

--- app/Exports/InvoicesExport.php
+++ app/Exports/InvoicesExport.php
@@ -1,66 +1,27 @@
 <?php
 
-// namespace App\Exports;
-
-// use App\Invoice;
-// use Illuminate\Contracts\Queue\ShouldQueue;
-// use Maatwebsite\Excel\Concerns\Exportable;
-// use Maatwebsite\Excel\Concerns\FromQuery;
-
-// class InvoicesExport implements FromQuery, ShouldQueue
-// {
-//     use Exportable;
-
-//     public function query()
-//     {
-//         return Invoice::query();
-//     }
 namespace App\Exports;
 
 use App\Invoice;
-use App\Product;
 use Illuminate\Support\Facades\DB;
-use Maatwebsite\Excel\Concerns\Exportable;
-use Maatwebsite\Excel\Concerns\FromCollection;
 use Maatwebsite\Excel\Concerns\FromQuery;
-use Maatwebsite\Excel\Concerns\ShouldAutoSize;
 use Maatwebsite\Excel\Concerns\WithHeadings;
-use Maatwebsite\Excel\Concerns\WithMapping;
+use Maatwebsite\Excel\Concerns\ShouldAutoSize;
 
-class InvoicesExport implements FromQuery, WithHeadings
+
+class InvoicesExport implements FromQuery , WithHeadings, ShouldAutoSize
 {
-    /*
-    *@var Invoice $invoice
-    */
-    use Exportable;
-
-    
-
-    
-
-    public function query(){
-
-
-        $invoices =  DB::table('invoices')
-            ->join('clients', 'invoices.client_id', '=', 'clients.id')
-            ->join('sellers', 'invoices.seller_id', '=', 'sellers.id')
-            ->select('invoices.client_id', 'clients.first_name', 'clients.last_name', 
-            'invoices.seller_id', 'sellers.email')
-            ->orderby('invoices.id');
-            
-            return $invoices;
-
-        // $invoice_product = DB::table('invoice_product')
-        // ->join('products', 'invoice_product.product_id', '=', 'products.id')
-        // ->join('invoices', 'invoice_product.invoice_id', '=', 'invoices.id')
-        // ->select('invoice_product.invoice_id','invoices.expedition_date',
-        //     'invoices.expiration_date','products.name','invoice_product.quantity',  'products.unit_value' ,'invoices.iva', 'invoices.total')
-        //     ->orderBy('invoice_product.invoice_id')
-            
-        //     ;
-
+    public function query()
+    {
+        $invoice_product = DB::table('invoice_product')
+        ->join('products', 'invoice_product.product_id', '=', 'products.id')
+        ->join('invoices', 'invoice_product.invoice_id', '=', 'invoices.id')
+        ->select('invoice_product.invoice_id','invoices.state', 'invoices.expedition_date',
+            'invoices.expiration_date','products.name','invoice_product.quantity', 'invoices.subtotal',
+             'invoices.iva', 'invoices.total')
+            ->orderBy('invoice_product.invoice_id');
         
-        //     return $invoice_product;
+        return $invoice_product;
     }
 
 
@@ -68,15 +29,19 @@ class InvoicesExport implements FromQuery, WithHeadings
     {
         return [
             'Factura ID',
+            'Estado',
             'Fecha Expedición',
             'Fecha Expiración',
             'Producto',
             'Cantidad',
-            'Valor Unitario',
-            'iva',
+            'SubTotal',
+            'Iva',
             'Total',
+            
             
         ];
     }
+
+    
     
 }
