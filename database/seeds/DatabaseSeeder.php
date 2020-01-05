96875fde76720954288efec5d8023a80dd01d9fa
William G. Navarro <williamnavarrocanon@gmail.com>
2020-01-04 19:57:53 -0500

    Create truncate functions

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,16 +13,20 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
 
-        
-        // DB::table('clients')->truncate();
-        // DB::table('sellers')->truncate();
-        // DB::table('products')->truncate();
-        // DB::table('invoices')->truncate();
-        // DB::table('invoice_product')->truncate();
-        
+        Schema::disableForeignKeyConstraints();        
+
+        DB::table('clients')->truncate();
+        DB::table('sellers')->truncate();
+        DB::table('products')->truncate();
+        DB::table('invoices')->truncate();
+        DB::table('invoice_product')->truncate();
+
+        Schema::enableForeignKeyConstraints();        
+
         factory(App\Client::class, 30)->create();
         factory(App\Seller::class, 30)->create();
         factory(App\Product::class, 30)->create();
+        
         
     }
 }
