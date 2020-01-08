ed2d8bc93cb714d85d3e7e11e8e6670eb7702e0c
William G. Navarro <williamnavarrocanon@gmail.com>
2020-01-07 21:16:08 -0500

    Add fillable invoice_products

--- app/Invoice_product.php
+++ app/Invoice_product.php
@@ -7,4 +7,10 @@ use Illuminate\Database\Eloquent\Model;
 class Invoice_product extends Model
 {
     protected $table = 'invoice_product';
+
+    protected $fillable = [
+        'quantity',
+        'invoice_id',
+        'product_id'
+    ];
 }
