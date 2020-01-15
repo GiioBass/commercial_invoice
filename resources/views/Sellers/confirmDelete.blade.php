ac61851d753ad8eb169d42e578fb58280e83b03c
William G. Navarro <williamnavarrocanon@gmail.com>
2020-01-14 22:56:55 -0500

    Updated Views Delete

--- resources/views/Sellers/confirmDelete.blade.php
+++ resources/views/Sellers/confirmDelete.blade.php
@@ -1,15 +1,25 @@
 @extends('template')
 @section('content')
-<div class="title" style="width: 30%; margin:50px auto;  text-align:center ">
-    <h1>ELIMINAR VENDEDOR</h1>
+
+<div class="content-form">
+    <div class="board-form">
+        <div class="content-title">
+            <div class="title">
+                <h1>ELIMINAR VENDEDOR</h1>
+            </div>
+        </div>
+        <form class="form" action="{{route('seller.destroy', $seller)}} " method="POST">
+            @method('DELETE')
+            @csrf
+            <div class="items-form">
+                <button class="button" type="submit">Eliminar</button>
+            </div>
+        </form>
+        <a class="item-menu" href="{{route('seller.index')}}">
+            <div class="item-button">
+                Atras
+            </div>
+        </a>
+    </div>
 </div>
-
-<form  action="{{route('seller.destroy', $seller)}}" method="POST">
-    @method('DELETE')
-    @csrf
-    <button style="display: block; text-decoration: none;" type="submit">Eliminar Cliente</button>
-</form>
-<a href="{{route('seller.index')}}">Atras</a>
-
-
 @endsection
