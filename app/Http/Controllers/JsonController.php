d296064db3aefdf9a9c42970b740fb81b811dd15
William G. Navarro <williamnavarrocanon@gmail.com>
2020-01-25 03:48:44 -0500

    Implementation new code redirection

--- app/Http/Controllers/JsonController.php
+++ app/Http/Controllers/JsonController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Invoice;
 use Dnetix\Redirection\PlacetoPay;
 
 class JsonController extends Controller
@@ -14,51 +15,140 @@ class JsonController extends Controller
         Endpoint: https://test.placetopay.com/redirection/
      * */
 
-    public function createJson()
+    public function createJson(Invoice $invoice)
     {
-       $placetopay = new PlacetoPay([
+
+
+        $placetopay = new PlacetoPay([
             'login' => '6dd490faf9cb87a9862245da41170ff2',
             'tranKey' => '024h1IlD',
             'url' => 'https://test.placetopay.com/redirection/',
         ]);
+        $reference = $invoice->id;
+//            'TEST_' . time();
 
-        $reference = 'TEST_' . time();
-
+// Request Information
         $request = [
-            'payer' => [
-                'name' => 'John',
-                'surname' => 'Doe',
-                'email' => 'john.doe@example.com',
-                'document' => '1040035000',
-                'documentType' => 'CC',
-                'mobile' => '3006108300'
+            "locale" => "es_CO",
+//            COMPRADOR
+            "payer" => [
+                "name" => $invoice->client->first_name,
+                "surname" => $invoice->client->last_name,
+                "email" => $invoice->client->email,
+                "documentType" => $invoice->client->document_type->documentType,
+                "document" => $invoice->client->id,
+                "mobile" => "3006108300",
+                "address" => $invoice->client->address
             ],
-            'payment' => [
-                'reference' => $reference,
-                'description' => 'Testing payment',
-                'amount' => [
-                    'currency' => 'COP',
-                    'total' => 120000,
+            "buyer" => [
+                "name" => "Julia",
+                "surname" => "Snow",
+                "email" => "julia@snow.com",
+                "documentType" => "CC",
+                "document" => "222222222",
+                "mobile" => "3006108300",
+                "address" => [
+                    "street" => "703 Dicki Island Apt. 609",
+                    "city" => "North Randallstad",
+                    "state" => "Antioquia",
+                    "postalCode" => "46292",
+                    "country" => "US",
+                    "phone" => "363-547-1441 x383"
+                ]
+            ],
+            "payment" => [
+                "reference" => $reference,
+                "description" => "Iusto sit et voluptatem.",
+                "amount" => [
+                    "taxes" => [
+                        [
+                            "kind" => "ice",
+                            "amount" => 0,
+                            "base" => 0
+                        ],
+                        [
+                            "kind" => "valueAddedTax",
+                            "amount" => 0,
+                            "base" => 0
+                        ]
+                    ],
+                    "details" => [
+                        [
+                            "kind" => "shipping",
+                            "amount" => 0
+                        ],
+
+                        [
+                            "kind" => "tip",
+                            "amount" => 0
+                        ],
+                        [
+                            "kind" => "subtotal",
+                            "amount" => $invoice->client->subTotal
+                        ]
+                    ],
+                    "currency" => "COP",
+                    "total" => $invoice->total
                 ],
+                "items" => [
+                    [
+                        "sku" => 26443,
+                        "name" => "Qui voluptatem excepturi.",
+                        "category" => "physical",
+                        "qty" => 1,
+                        "price" => 940,
+                        "tax" => 89.3
+                    ]
+                ],
+                "shipping" => [
+                    "name" => "Violet",
+                    "surname" => "Rose",
+                    "email" => "violer@rose.com",
+                    "documentType" => "CC",
+                    "document" => "11111111111",
+                    "mobile" => "3006108300",
+                    "address" => [
+                        "street" => "703 Dicki Island Apt. 609",
+                        "city" => "North Randallstad",
+                        "state" => "Antioquia",
+                        "postalCode" => "46292",
+                        "country" => "US",
+                        "phone" => "363-547-1441 x383"
+                    ]
+                ],
+                "allowPartial" => false
             ],
-            'expiration' => date('c', strtotime('+2 days')),
-            'returnUrl' => route('invoice.show', 1) ,
-//            . $reference,
-            'ipAddress' => '127.0.0.1',
-            'userAgent' => 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.116 Safari/537.36',
+            "expiration" => date('c', strtotime('+1 hour')),
+            "ipAddress" => "127.0.0.1",
+            "userAgent" => "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.86 Safari/537.36",
+            "returnUrl" => route('invoice.show', $invoice->id),
+            "cancelUrl" => route('invoice.show', $invoice->id),
+            "skipResult" => false,
+            "noBuyerFill" => false,
+            "captureAddress" => false,
+            "paymentMethod" => null
         ];
 
-        $response = $placetopay->request($request);
-        if ($response->isSuccessful()) {
-            // STORE THE $response->requestId() and $response->processUrl() on your DB associated with the payment order
-            // Redirect the client to the processUrl or display it on the JS extension
-            header('Location: ' . $response->processUrl());
-        } else {
-            // There was some error so check the message and log it
-            $response->status()->message();
+        try {
+//            $placetopay = placetopay();
+            $response = $placetopay->request($request);
+            if ($response->isSuccessful()) {
+                // STORE THE $response->requestId() and $response->processUrl() on your DB associated with the payment order
+                // Redirect the client to the processUrl or display it on the JS extension
+                header('Location: ' . $response->processUrl());
+            } else {
+                // There was some error so check the message and log it
+                $response->status()->message();
+            }
+            var_dump($response);
+        } catch (Exception $e) {
+            var_dump($e->getMessage());
         }
 
-        dd($placetopay);
+
+//        TODO ERROR!!!
+        dd();
+
     }
 
 
