31931a190f30b4c30d893831e0578108e0dc26d8
William G. Navarro <williamnavarrocanon@gmail.com>
2020-01-25 00:23:04 -0500

    Added relationship document_type

--- app/DocumentType.php
+++ app/DocumentType.php
@@ -6,4 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class DocumentType extends Model
 {
+    protected $primaryKey = 'id';
+    protected $table = 'document_types';
+
+    public function clients()
+    {
+        return $this->hasMany(Client::class);
+
+    }
 }
