f45e2665fe224ad94fcbb00205bf63dacb3f95a8
William G. Navarro <williamnavarrocanon@gmail.com>
2020-01-31 18:33:25 -0500

    Create ControlReportInvoiceController.php

---
+++ app/Http/Controllers/ControlReportInvoiceController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\ControlReportInvoice;
+use Illuminate\Http\Request;
+
+class ControlReportInvoiceController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\ControlReportInvoice  $controlReportInvoice
+     * @return \Illuminate\Http\Response
+     */
+    public function show(ControlReportInvoice $controlReportInvoice)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\ControlReportInvoice  $controlReportInvoice
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(ControlReportInvoice $controlReportInvoice)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\ControlReportInvoice  $controlReportInvoice
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, ControlReportInvoice $controlReportInvoice)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\ControlReportInvoice  $controlReportInvoice
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(ControlReportInvoice $controlReportInvoice)
+    {
+        //
+    }
+}
