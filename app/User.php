a8404cba216ea038331559af36284da483834121
William G. Navarro <williamnavarrocanon@gmail.com>
2020-03-02 21:04:03 -0500

    Update User HasRolesAndPermissions attribute

--- app/User.php
+++ app/User.php
@@ -2,12 +2,14 @@
 
 namespace App;
 
+use Caffeinated\Shinobi\Concerns\HasRolesAndPermissions;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
     use Notifiable;
+    use HasRolesAndPermissions;
 
     /**
      * The attributes that are mass assignable.
@@ -35,4 +37,5 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+    private $id;
 }
