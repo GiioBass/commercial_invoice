7d617eb52633b25dbf832269c4d656aa1ab1326f
William G. Navarro <williamnavarrocanon@gmail.com>
2020-03-12 16:58:10 -0500

    Update messages and validation

--- app/Http/Controllers/api/ApiClientController.php
+++ app/Http/Controllers/api/ApiClientController.php
@@ -4,14 +4,16 @@ namespace App\Http\Controllers\api;
 
 use App\Client;
 use App\Http\Controllers\Controller;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 class ApiClientController extends Controller
 {
     /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return Client[]|Collection
      */
     public function index()
     {
@@ -21,7 +23,7 @@ class ApiClientController extends Controller
     /**
      * Show the form for creating a new resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return void
      */
     public function create()
     {
@@ -31,19 +33,19 @@ class ApiClientController extends Controller
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
+     * @param Request $request
+     * @return Response
      */
     public function store(Request $request)
     {
         $validData = $request->validate([
-            'id' => 'required',
-            'document_type_id' => 'required',
-            'first_name' => 'required',
-            'last_name' => 'required',
-            'phone_number' => 'required',
-            'address' => 'required',
-            'email' => 'required'
+            'id' => 'required|numeric',
+            'document_type_id' => 'required|numeric',
+            'first_name' => 'required|string',
+            'last_name' => 'required|string',
+            'phone_number' => 'required|numeric',
+            'address' => 'required|string',
+            'email' => 'required|email:rfc'
         ]);
 
         $client = new Client();
@@ -53,10 +55,10 @@ class ApiClientController extends Controller
         $client->last_name = $validData['last_name'];
         $client->phone_number = $validData['phone_number'];
         $client->address = $validData['address'];
-        $client->email  = $validData['email'];
+        $client->email = $validData['email'];
         $client->save();
 
-        return response(['message' => 'Cliente Almacenado']);
+        return response(['message' => 'Cliente Añadido']);
 
 
     }
@@ -64,19 +66,19 @@ class ApiClientController extends Controller
     /**
      * Display the specified resource.
      *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @param int $id
+     * @return Response
      */
     public function show($id)
     {
-        //
+        return Client::findOrFail($id);
     }
 
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @param int $id
+     * @return void
      */
     public function edit($id)
     {
@@ -86,45 +88,47 @@ class ApiClientController extends Controller
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @param Request $request
+     * @param int $id
+     * @return Response
      */
     public function update(Request $request, $id)
     {
+
         $validData = $request->validate([
-            'id' => 'required',
-            'document_type_id' => 'required',
-            'first_name' => 'required',
-            'last_name' => 'required',
-            'phone_number' => 'required|numeric',
-            'email' => 'requires|email:rfc,dns',
-            'address' => 'required',
+           'id' => 'required|numeric',
+           'document_type_id' => 'required|numeric',
+           'first_name' => 'required|string',
+           'last_name' => 'required|string',
+           'phone_number' => 'required|numeric',
+           'address' => 'required|string',
+           'email' => 'required|email:rfc',
+
         ]);
 
-        $client = Client::findOrFail($id);
+        $client = Client::find($id);
         $client->id = $validData['id'];
         $client->document_type_id = $validData['document_type_id'];
         $client->first_name = $validData['first_name'];
         $client->last_name = $validData['last_name'];
         $client->phone_number = $validData['phone_number'];
-        $client->email = $validData['email'];
         $client->address = $validData['address'];
-
+        $client->email = $validData['email'];
         $client->save();
-        return redirect()->route('client.index');
+
+        return response(['message' => 'Cliente Actualizado']);
     }
 
     /**
      * Remove the specified resource from storage.
      *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @param int $id
+     * @return Response
      */
     public function destroy($id)
     {
         $client = Client::findOrFail($id);
         $client->delete();
-        return redirect()->route('client.index');
+        return response(['message' => 'Cliente Eliminado']);
     }
 }
