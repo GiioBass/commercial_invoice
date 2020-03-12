8da7619a60efbf41c43b2ba727e59377dffb0728
William G. Navarro <williamnavarrocanon@gmail.com>
2020-03-12 16:59:36 -0500

    update message, added function update.

--- app/Http/Controllers/api/ApiProductController.php
+++ app/Http/Controllers/api/ApiProductController.php
@@ -3,24 +3,27 @@
 namespace App\Http\Controllers\api;
 
 use App\Http\Controllers\Controller;
+use App\Product;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 class ApiProductController extends Controller
 {
     /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return Product[]|Collection
      */
     public function index()
     {
-        //
+        return Product::all();
     }
 
     /**
      * Show the form for creating a new resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return void
      */
     public function create()
     {
@@ -30,30 +33,44 @@ class ApiProductController extends Controller
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
+     * @param Request $request
+     * @return void
      */
     public function store(Request $request)
     {
-        //
+        $validData = $request->validate([
+            'id' => 'required|numeric',
+            'name' => 'required',
+            'description' => 'required',
+            'unit_value' => 'required|numeric',
+        ]);
+
+        $product = new Product;
+        $product->id = $validData['id'];
+        $product->name = $validData['name'];
+        $product->description = $validData['description'];
+        $product->unit_value = $validData['unit_value'];
+
+        $product->save();
+        return response(['message' => 'Producto Añadido']);
     }
 
     /**
      * Display the specified resource.
      *
      * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @return Response
      */
     public function show($id)
     {
-        //
+        return Product::finOrFail($id);
     }
 
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @param int $id
+     * @return void
      */
     public function edit($id)
     {
@@ -63,23 +80,39 @@ class ApiProductController extends Controller
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @param Request $request
+     * @param int $id
+     * @return void
      */
     public function update(Request $request, $id)
     {
-        //
+        $validData = $request->validate([
+            'id' => 'required|numeric',
+            'name' => 'required',
+            'description' => 'required',
+            'unit_value' => 'required|numeric',
+        ]);
+
+        $product = new Product;
+        $product->id = $validData['id'];
+        $product->name = $validData['name'];
+        $product->description = $validData['description'];
+        $product->unit_value = $validData['unit_value'];
+
+        $product->save();
+        return response(['message' => 'Producto Actualizado']);
     }
 
     /**
      * Remove the specified resource from storage.
      *
      * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @return Response
      */
     public function destroy($id)
     {
-        //
+        $product = Product::findOrFail($id);
+        $product->delete();
+        return response(['message' => 'Producto Eliminado']);
     }
 }
