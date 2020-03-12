669dcf4188b208b85ea778d60ac3168c4c708dd1
William G. Navarro <williamnavarrocanon@gmail.com>
2020-03-12 13:04:11 -0500

    Create control_panel.blade.php

---
+++ resources/views/Passport/control_panel.blade.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Ibarra+Real+Nova&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="{{asset('css/home.css')}}">
+    <title>INICIO</title>
+</head>
+
+<body>
+
+<div class="main">
+    <div class="container">
+        <div class="login-nav">
+            <div>
+                <i class="material-icons icon">account_circle</i>
+            </div>
+            <div>
+                <p class="item">{{ Auth::user()->name }}</p>
+            </div>
+            <div>
+                <a href="{{route('logout')}}"
+                   onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                    <i class="material-icons icon">exit_to_app</i>
+                </a>
+                <form id="logout-form" action="{{ route('logout') }}" method="POST">
+                    @csrf
+                </form>
+            </div>
+        </div>
+        <div class="container-title">
+            <h1 class="title">Control de Accesos Api</h1>
+        </div>
+
+
+        <div class="container-content">
+            <div id="app">
+                <passport-personal-access-tokens></passport-personal-access-tokens>
+            </div>
+        </div>
+
+    </div>
+</div>
+</body>
+<script src="{{asset('js/app.js')}}"></script>
+</html>
+
