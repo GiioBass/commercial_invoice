ec972ab7860ff446f5bc8cf2f8b582dafbcc55b3
William G. Navarro <williamnavarrocanon@gmail.com>
2020-03-12 20:49:24 -0500

    added permission access api  / view

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -66,12 +66,14 @@
                     </li>
                 </a>
             @endcan
-            <a class="title-button" href="{{route('access_api')}}">
-                <i class="material-icons">person_outline</i>
-                <li class="link">
-                    Accesos API
-                </li>
-            </a>
+            @can('access_api')
+                <a class="title-button" href="{{route('access_api')}}">
+                    <i class="material-icons">person_outline</i>
+                    <li class="link">
+                        Accesos API
+                    </li>
+                </a>
+            @endcan
 
         </ul>
     </div>
