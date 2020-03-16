59a4b877cdaa6fd36d4768ef12d8ff6400454d72
William G. Navarro <williamnavarrocanon@gmail.com>
2020-03-16 00:25:59 -0500

    Create UserAdminSeeder.php

---
+++ database/seeds/UserAdminSeeder.php
@@ -0,0 +1,45 @@
+<?php
+
+use Caffeinated\Shinobi\Models\Permission;
+use Caffeinated\Shinobi\Models\Role;
+use Illuminate\Database\Seeder;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class UserAdminSeeder extends Seeder
+{
+    use RefreshDatabase;
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+
+        Schema::disableForeignKeyConstraints();
+
+        DB::table('users')->truncate();
+        DB::table('role_user')->truncate();
+        DB::table('roles')->truncate();
+
+        Schema::enableForeignKeyConstraints();
+
+
+        DB::table('users')->insert([
+            'name' => 'admin',
+            'email' => 'admin@laravel.com',
+            'password' => Hash::make('12345678'),
+        ]);
+
+        Role::create([
+            'name' => 'Admin',
+            'slug' => 'admin',
+            'special' => 'all-access'
+        ]);
+
+        DB::table('role_user')->insert([
+            'role_id' => 1,
+            'user_id' => 1
+        ]);
+    }
+}
