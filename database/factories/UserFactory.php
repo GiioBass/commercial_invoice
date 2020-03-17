b7af31dc49757b7259eae165d1866ac76a284907
William G. Navarro <williamnavarrocanon@gmail.com>
2020-03-16 20:16:07 -0500

    Update UserFactory.php

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -7,6 +7,7 @@ use Faker\Generator as Faker;
 use Illuminate\Database\Eloquent\Factory;
 use Illuminate\Support\Str;
 
+
 /*
 |--------------------------------------------------------------------------
 | Model Factories
@@ -19,5 +20,10 @@ use Illuminate\Support\Str;
 */
 
 $factory->define(User::class, function (Faker $faker) {
-//
+
+    return [
+            'name' => $faker->name,
+            'email' => $faker->safeEmail,
+            'password' => Hash::make($faker->password),
+        ];
 });
