57c21b1a86f6806882675a3666290c0ca24fa101
William G. Navarro <williamnavarrocanon@gmail.com>
2020-03-19 23:37:03 -0500

    remove code

--- app/Imports/FirstSheetImport.php
+++ app/Imports/FirstSheetImport.php
@@ -16,7 +16,7 @@ class FirstSheetImport implements ToModel, WithValidation, WithHeadingRow
     public function model(Array $row)
     {
         return new Invoice([
-                'code' => $row['code'],
+                'id' => $row['id'],
                 'state' => $row['state'],
                 'expedition_date' => $row['expedition_date'],
                 'expiration_date' => $row['expiration_date'],
@@ -31,7 +31,7 @@ class FirstSheetImport implements ToModel, WithValidation, WithHeadingRow
     public function rules(): array
     {
         return[
-                'code' => 'required|numeric',
+                'id' => 'required|numeric',
                 'state' => 'required|string',
                 'expedition_date' => 'required|date',
                 'expiration_date' => 'required|date',
@@ -46,25 +46,25 @@ class FirstSheetImport implements ToModel, WithValidation, WithHeadingRow
     public function customValidationMessages()
     {
         return [
-                'code.required' => 'El código es requerido.',
-                'code.numeric' => 'El código debe ser númerico.',
+                'id.required' => 'El id de la factura es requerido.',
+                'id.numeric' => 'El id de la factura debe ser númerico.',
                 'state.required' => 'El Estado de la factura es requerido.',
-                'state.string' => 'El Estado debe ser "Pago" o "Por Pagar".',
-                'expedition_date.required' => 'La fecha de expedición es requerido.',
-                'expedition_date.date' => 'La fecha de expedición no tiene el formato correcto.',
-                'expiration.required' => 'La fecha de expiración es requerido.',
-                'expiration.date' => 'La fecha de expiración no tiene el formato correcto.',
-                'seller_id.required' => 'El codigo del vendedor es requerido.',
-                'seller_id.numeric' => 'El codigo del vendedor debe ser númerico.',
-                'client.required' => 'El codigo del cliente es requerido.',
-                'client.numeric' => 'El codigo del cliente debe ser númerico.',
+                'state.string' => 'El Estado de la factura debe ser "Pago" o "Por Pagar".',
+                'expedition_date.required' => 'La fecha de expedición de la factura es requerido.',
+                'expedition_date.date' => 'La fecha de expedición de la factura no tiene el formato correcto.',
+                'expiration.required' => 'La fecha de expiración de la factura es requerido.',
+                'expiration.date' => 'La fecha de expiración de la factura no tiene el formato correcto.',
+                'seller_id.required' => 'El codigo del vendedor de la factura es requerido.',
+                'seller_id.numeric' => 'El codigo del vendedor de la factura debe ser númerico.',
+                'client.required' => 'El codigo del cliente de la factura es requerido.',
+                'client.numeric' => 'El codigo del cliente de la factura debe ser númerico.',
             ];
     }
 
-    public function customValidationAttributes()
+    /*public function customValidationAttributes()
     {
         return[
-                'code' => 'codigo',
+                'id' => 'codigo',
                 'state' => 'estado',
                 'expedition_date' => 'fecha de expedicion',
                 'expiration_date' => 'fecha de expiracion',
@@ -74,5 +74,5 @@ class FirstSheetImport implements ToModel, WithValidation, WithHeadingRow
                 'seller_id' => 'vendedor',
                 'client_id' => 'cliente',
             ];
-    }
+    }*/
 }
